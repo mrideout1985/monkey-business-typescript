60ce2264da9e683a75b0d8d4b25f140204818f6b
mrideout1985 <mrideout1985@gmail.com>
2022-01-02 21:09:04 +0000

    added hook & removed default hooks

---
+++ hooks/useGetMonkeys.ts
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { RootObject } from '../interfaces/interfaces';
+
+const useGetMonkeys = () => {
+  const [monkeyList, setMonkeyList] = useState<RootObject>();
+
+  useEffect(() => {
+    axios
+      .get('https://dev-cloud.cc/api/')
+      .then((res) => {
+        setMonkeyList(res.data);
+      })
+      .catch((error) => {
+        throw new Error(error);
+      });
+  }, []);
+
+  return monkeyList;
+};
+
+export { useGetMonkeys };
