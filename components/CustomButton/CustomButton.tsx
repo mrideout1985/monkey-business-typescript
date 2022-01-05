cab2fd8fb84d3bc5b757a30e0628c4de468239a7
mrideout1985 <mrideout1985@gmail.com>
2022-01-05 04:38:14 +0000

    styling & props changes to custom components

--- components/CustomButton/CustomButton.tsx
+++ components/CustomButton/CustomButton.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Text, TouchableOpacity, StyleSheet } from 'react-native';
+import React from "react";
+import { Text, TouchableOpacity, StyleSheet } from "react-native";
 
 interface CustomButtonProps {
   onPress?: () => void;
@@ -9,23 +9,26 @@ interface CustomButtonProps {
 export const buttonStyles = StyleSheet.create({
   buttonContainer: {
     elevation: 12,
-    backgroundColor: '#1d1716',
+    backgroundColor: "#1d1716",
     borderRadius: 25,
     paddingVertical: 10,
     margin: 10,
-    width: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
   },
   btnText: {
     fontSize: 18,
-    fontFamily: 'Poppins-Regular',
-    color: '#fdf6f6',
-    textTransform: 'uppercase',
+    fontFamily: "Poppins-Regular",
+    color: "#fdf6f6",
+    textTransform: "uppercase",
   },
 });
 
-export default function CustomButton({ onPress, btnText }: CustomButtonProps): JSX.Element {
+export default function CustomButton({
+  onPress,
+  btnText,
+}: CustomButtonProps): JSX.Element {
   return (
     <TouchableOpacity style={buttonStyles.buttonContainer} onPress={onPress}>
       <Text style={buttonStyles.btnText}>{btnText}</Text>
