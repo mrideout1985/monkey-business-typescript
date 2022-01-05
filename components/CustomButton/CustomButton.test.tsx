555f4e443cadb0f229ebc3916da1e989c58d35ae
mrideout1985 <mrideout1985@gmail.com>
2022-01-05 23:29:15 +0000

    renamed variable

--- components/CustomButton/CustomButton.test.tsx
+++ components/CustomButton/CustomButton.test.tsx
@@ -3,14 +3,16 @@ import { render, fireEvent } from "@testing-library/react-native";
 import CustomButton from "./CustomButton";
 
 test("it renders <CustomButton/>", () => {
-  render(<CustomButton btnText="Hello" />);
+    render(<CustomButton btnText="Hello" />);
 });
 
 test("onPress gets called", () => {
-  const text = "Button";
-  const abc = jest.fn();
-  const { getByText } = render(<CustomButton btnText={text} onPress={abc} />);
-  const button = getByText(text);
-  fireEvent.press(button);
-  expect(abc).toBeCalled();
+    const text = "Button";
+    const mockFunction = jest.fn();
+    const { getByText } = render(
+        <CustomButton btnText={text} onPress={mockFunction} />,
+    );
+    const button = getByText(text);
+    fireEvent.press(button);
+    expect(mockFunction).toBeCalled();
 });
