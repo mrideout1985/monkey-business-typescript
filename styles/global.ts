3818127f6c944fb5c773a591d558920da0410fe9
mrideout1985 <mrideout1985@gmail.com>
2022-01-05 04:35:36 +0000

    styling

--- styles/global.ts
+++ styles/global.ts
@@ -1,16 +1,15 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 export const globalStyles = StyleSheet.create({
   container: {
-    backgroundColor: '#2e2b2b',
     flex: 1,
   },
   paragraph: {
     marginVertical: 8,
     padding: 25,
     fontSize: 16,
-    fontFamily: 'Poppins-Regular',
+    fontFamily: "Poppins-Regular",
     lineHeight: 20,
-    color: '#fff',
+    color: "black",
   },
 });
