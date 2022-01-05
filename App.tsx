3e1dc46de714e3abede9bf400ff31cc8fac30a57
mrideout1985 <mrideout1985@gmail.com>
2022-01-05 04:37:17 +0000

    added navigation containers to APP.tsx

--- App.tsx
+++ App.tsx
@@ -1,16 +1,23 @@
 /* eslint-disable global-require */
-import React from 'react';
-import { useFonts } from 'expo-font';
-import AppLoading from 'expo-app-loading';
-import HomeStack from './routes/homeStack';
+import React, { useEffect, useState } from "react";
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
+import { NavigationContainer } from "@react-navigation/native";
+import { View } from "react-native";
+import { TabStack } from "./routes/TabStack";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    'Poppins-Regular': require('./assets/fonts/Poppins-Regular.ttf'),
-    'Poppins-Bold': require('./assets/fonts/Poppins-Bold.ttf'),
+    "Poppins-Regular": require("./assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-Bold": require("./assets/fonts/Poppins-Bold.ttf"),
   });
+
   if (!fontsLoaded) {
     return <AppLoading />;
   }
-  return <HomeStack />;
+  return (
+    <NavigationContainer>
+      <TabStack />
+    </NavigationContainer>
+  );
 }
