b8235e87a831af24debbd94437d1979e6c4d26d4
mrideout1985 <mrideout1985@gmail.com>
2022-01-07 04:21:40 +0000

    refactored for cleaner code

---
+++ assets/data/homeText.ts
@@ -0,0 +1,7 @@
+export const homeText = `Monkey Business is a directory of Monkeys in a zoo. Users
+are able to view a list of monkeys along with their age and
+the number of bananas they have. When you click a Monkey it
+will then show their information in a profile style view
+along with a photo of the Monkey. Please click the
+&quot;Monkeys&quot; tab, or the icon below to view our
+monkeys.`;
--- screens/home.tsx
+++ screens/home.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text, Image, TouchableOpacity } from "react-native";
+import { homeText } from "../assets/data/homeText";
 import Container from "../components/Container/Container";
 import Header from "../components/Header/Header";
 import { HomeProps } from "../interfaces/types";
@@ -10,15 +11,7 @@ export default function Home({ navigation }: HomeProps<"Monkeys">) {
         <View style={homeStyles.container}>
             <Header title="Monkey Business" color="#d3d6db" />
             <Container backgroundColor="#d3d6db">
-                <Text style={homeStyles.text}>
-                    Monkey Business is a directory of Monkeys in a zoo. Users
-                    are able to view a list of monkeys along with their age and
-                    the number of bananas they have. When you click a Monkey it
-                    will then show their information in a profile style view
-                    along with a photo of the Monkey. Please click the
-                    &quot;Monkeys&quot; tab, or the icon below to view our
-                    monkeys.
-                </Text>
+                <Text style={homeStyles.text}>{homeText}</Text>
                 <TouchableOpacity
                     style={homeStyles.monkey}
                     onPress={() => navigation.navigate("Monkeys")}
